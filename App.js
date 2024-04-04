42e36e63c020d7b9d1b7b9d90d162d513da846ae
Vedantgarg2001 <gargvedant001@gmail.com>
2024-04-04 21:23:19 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+
+const heading=React.createElement("h1",{id:"heading"},"ydgsy heldbdhddjdhit okfddddy")
+
+        const root=ReactDOM.createRoot(document.getElementById("root"));
+        root.render(heading);
