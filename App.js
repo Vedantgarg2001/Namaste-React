8937bb7010072b93d0afa8f469a7108aa634fc67
Vedantgarg2001 <gargvedant001@gmail.com>
2024-04-04 23:36:13 +0530

    second commit

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 
-const heading=React.createElement("h1",{id:"heading"},"ydgsy heldbdhddjdhit okfddddy")
+const heading=React.createElement("h1",{id:"heading"},"ydgsy heldbhhfhdhddjdhit okfddddy")
 
         const root=ReactDOM.createRoot(document.getElementById("root"));
         root.render(heading);
